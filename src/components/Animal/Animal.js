31097f0e6ab726c73ddf2453880b2d1ffe10ea76
brenley-woodard <brenleyew@gmail.com>
2023-01-30 09:33:05 -0800

    this did not make me feel better

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,7 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div className='animal' style={{ top: props.top, left: props.left }}>
+      <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`}/>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import Animal from '../Animal/Animal';
+import { animals } from '../../data';
+import background from '../../background.png';
 import './Main.css';
 
 export default function Main() {
-  return <main></main>;
+  return <main style={{ backgroundImage: `url(${background})` }}>
+    {animals.map((prop) => (
+      <Animal key={prop.name} {...prop} />
+    ))}
+  </main>;
 }
